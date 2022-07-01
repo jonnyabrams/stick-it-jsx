6b862f6c0df5d1fdc94abf31495498a273c0f7c0
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-01 11:09:38 +0100

    Add note form and button with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
 function App() {
+
+  const addNote = () => {
+
+  }
   return (
     <div className="App">
        <h1>Stick It</h1>
+       <form onSubmit={addNote} className='note-form'>
+         <textarea placeholder="Get sticky?"></textarea>
+         <button>Add</button>
+       </form>
     </div>
   );
 }
